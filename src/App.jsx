31c783a45e073150afafdc4984dcf81a4b1ab9aa
Adnan Khan <adnankhannad@gmail.com>
2024-01-12 23:36:43 +0500

    Add NoProjecctSelected, and add a form to NewProject

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSideBar from "./components/ProjectSideBar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar />
+      <NoProjectSelected />
     </main>
   );
 }
